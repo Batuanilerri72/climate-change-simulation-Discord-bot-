5abfda5dacd97dbe123588d3ce5daf8005dec584
Jilly <167562149+Batuanilerri72@users.noreply.github.com>
2026-03-28 19:32:46 +0300

    backpack.py added
    Added information about climate change and a bot token.

---
+++ backpack.py
@@ -0,0 +1,15 @@
+TOKEN1 = "I can't let you see my token"
+
+
+Bilgiler = [
+    "İklim değişikliği, Dünya'nın ortalama sıcaklıklarında ve iklimlerin uzun vadeli (on yıllar-yüzyıllar) değişiklikler olmasıdır.",
+    "Fosil yakıtların (kömür, petrol, doğal gaz) yakılması (Karbondioksit salınımı) iklim değişikliğini güçlendirir.",
+    "Ormanlar karbondioksidi oksijene çevirir yani ormansızlaştırmak iklim değişikliğine yol açar.",
+    "Sera gazları atmosfere ısıyı hapseder ve iklim değişikliğine neden olur.",
+    "İklim değişikliği buzulların erimesine neden olur.",
+    "İklim değişikliği canlı türlerinin yok olmasına neden olur.",
+    "Yenilenebilir enerji (Rüzgar, dalga vs.) kullanmak ve enerjiyi verimli kullanmak iklim değişikliğini azaltır.",
+    "Ormanlaştırma ve ormanları korumak iklim değişikliğini önlemeye yardımcı olur.",
+    "Bireysel olarak iklim değişikliğine karşı yapabileceklerimiz: Özel araç kullanımını azaltmak, enerji tasarufu yapmak ve geri dönüşüm yapmak."
+]
+#this file contains the information to be printed in a command and the bot token
